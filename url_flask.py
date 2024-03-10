5fc721afa6203aa75c6a5ac3d56dee9c22f34bb9
ANSHU <143438304+anshusingh728@users.noreply.github.com>
2024-03-11 02:25:16 +0530

    url_flask.py

--- url_flask.py
+++ url_flask.py
@@ -16,7 +16,7 @@ app.secret_key = 'your_secret_key_here'  # Set the secret key used by Flask to s
 conn = psycopg2.connect(dbname='news_database', user='news_database_user', password='Ix6xhxuSTz6qGh936wnPMKJwA4CeLs52', host='dpg-cnmogjq1hbls739hkmtg-a')
 cur = conn.cursor()
 
-ADMIN_PASSWORD = "admin@728"  # Password required to access the admin panel.
+ADMIN_PASSWORD = "admin@728http"  # Password required to access the admin panel.
 cur.execute('''CREATE TABLE IF NOT EXISTS url_data(
             url TEXT PRIMARY KEY,
             news_heading TEXT,
